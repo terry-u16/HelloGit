1644c8509651a465279037492a28e7d9801c14fe
terry-u16 <gray_labo@hotmail.co.jp>
2015-09-23 01:13:24 +0900

    [Add] Add Taro

--- GitHubTest/Program.cs
+++ GitHubTest/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
 
 namespace GitHubTest
 {
@@ -11,6 +12,9 @@ namespace GitHubTest
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, GitHub!");
+            var taro = new Person() { Age = 24, Name = "Taro" };
+            var json = JsonConvert.SerializeObject(taro);
+            Console.WriteLine(json);
         }
     }
 }
